ae59da8a29a19676d46044663c54fad02ad78667
Alex Sherman <asherman.ca@gmail.com>
2023-10-04 10:42:55 -0700

    useList

---
+++ app/(pages)/list/page.tsx
@@ -0,0 +1,14 @@
+import Container from "@/app/components/Container";
+import { FC } from "react";
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+  return (
+    <Container>
+      <h1>List Title</h1>
+    </Container>
+  );
+};
+
+export default page;
---
+++ app/hooks/useList.tsx
@@ -0,0 +1,45 @@
+import * as React from "react";
+
+const placeholder = () => {};
+
+export default function useList(defaultList = []) {
+  const [list, setList] = React.useState(defaultList);
+
+  const set = (newList) => {
+    setList(newList);
+  };
+
+  const push = () => {
+    setList((list) => [...list, item]);
+  };
+
+  const removeAt = (idx) => {
+    setList((list) => {
+      const newList = [...list];
+      newList.splice(idx, 1);
+      return newList;
+    });
+  };
+
+  const insertAt = (idx, ele) => {
+    setList((list) => {
+      const newList = [...list];
+      newList.splice(idx, 0, ele);
+      return newList;
+    });
+  };
+
+  const updateAt = (idx, ele) => {
+    setList((list) => {
+      let newList = [...list]
+      newList[idx] = ele
+      return newList
+    }
+  };
+
+  const clear = () => {
+    setList([])
+  }
+
+  return [list, { set, push, removeAt, insertAt, updateAt, clear }];
+}
